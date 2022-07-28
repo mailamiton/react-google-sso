aa63784e10a0c033aef6a7ee04439949210b84fa
Amit Kumar <mailamiton@gmail.com>
2022-07-28 17:01:05 +0530

    inital commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import { GoogleAuth } from "./GoogleAuth";
+
+function App() {
+  return (
+    <div className="App">
+     <GoogleAuth></GoogleAuth>
+    </div>
+  );
+}
+
+export default App;
